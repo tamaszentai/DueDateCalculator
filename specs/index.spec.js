f0b67eea21b1a2398847d9de714630c85f97faab
Tamas Zentai <zentaitamas@gmail.com>
2021-06-28 12:41:17 +0100

    add isOutOfWorkingHours tests

--- specs/index.spec.js
+++ specs/index.spec.js
@@ -5,17 +5,30 @@ const index = require("../index");
 
 const notWeekend = new Date(Date.UTC(2021, 5, 28, 16, 59));
 const weekend = new Date(Date.UTC(2021, 5, 26, 16, 59));
+const outOfWorkingHours = new Date(Date.UTC(2021, 5, 28, 8, 59));
+const inWorkingHours = new Date(Date.UTC(2021, 5, 28, 16, 59));
 
 // Tests
 
 describe("isWeekend function", () => {
-    it("should returns true", () => {
-      const result = index.isWeekend(weekend);
-      assert.strictEqual(result, true);
-    });
-  
-    it("should returns false", () => {
-      const result = index.isWeekend(notWeekend);
-      assert.strictEqual(result, false);
-    });
+  it("should returns true", () => {
+    const result = index.isWeekend(weekend);
+    assert.strictEqual(result, true);
   });
+
+  it("should returns false", () => {
+    const result = index.isWeekend(notWeekend);
+    assert.strictEqual(result, false);
+  });
+});
+
+describe("isOutOfWorkingHours function", () => {
+  it("it should returns true", () => {
+    const result = index.isOutOfWorkingHours(outOfWorkingHours);
+    assert.strictEqual(result, true);
+  });
+  it("it should returns false", () => {
+    const result = index.isOutOfWorkingHours(inWorkingHours);
+    assert.strictEqual(result, false);
+  });
+});
