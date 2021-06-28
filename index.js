b353358033c0aef1fd91b6ed5bbecc888b8ec324
Tamas Zentai <zentaitamas@gmail.com>
2021-06-28 12:57:03 +0100

    add isOutOfWorkingHours

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 // Function for checking the date is on weekend or not
 
 const isWeekend = (date) => {
-    if (date.getUTCDay() === 6 || date.getUTCDay() === 0) {
-        return true
-    } else {
-        return false;
-    }
-}
+  if (date.getUTCDay() === 6 || date.getUTCDay() === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// Function for checking the time is in out of hours or not
+
+const isOutOfWorkingHours = (date) => {
+  if (date.getUTCHours() < 9 || date.getUTCHours() >= 17) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 exports.isWeekend = isWeekend;
+exports.isOutOfWorkingHours = isOutOfWorkingHours;
